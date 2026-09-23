cfc13cd6d268922c9d698644fe79ec8c5115fb0d
lior grossman <lior.grosman@gmail.com>
2026-09-24 01:48:50 +0300

    Add ImageCacheService and related registrations

--- src/WhatsAppDockerManager/Program.cs
+++ src/WhatsAppDockerManager/Program.cs
@@ -33,6 +33,10 @@ builder.Services.AddSingleton<IWebhookDispatcherService, WebhookDispatcherServic
 builder.Services.AddHostedService<HeartbeatService>();
 builder.Services.AddHostedService<TemplateStatusSyncService>();
 
+builder.Services.AddSingleton<ImageCacheService>();
+builder.Services.AddSingleton<IImageCacheService>(sp => sp.GetRequiredService<ImageCacheService>());
+builder.Services.AddHostedService(sp => sp.GetRequiredService<ImageCacheService>());
+
 builder.Services.AddHttpClient();
 
 builder.Services.AddControllers()
